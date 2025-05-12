8da8b5b5c0e4520d886a1a90f5cabc8242d6faa3
Camacho Lima Angel Emmanuel <angelemmanuelcamacho0@gmail.com>
2025-05-12 07:57:25 -0600

    ORIGINAL

---
+++ area triangulo.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+def operacion():
+    num1=float(entrada.get())
+    num2=float(entrada2.get())
+    area=(num1*num2)/2
+    etiqueta_resultado.config(text=f"el area es: {area}") 
+ventana=tk.Tk()
+ventana.title("Area del triangulo :)")
+ventana.geometry("400x300")
+ventana.configure(bg="red")
+etiqueta=tk.Label(ventana, text="Dame tu base: ", bg="red")
+etiqueta.pack()
+entrada=tk.Entry(ventana)
+entrada.pack()
+etiqueta2=tk.Label(ventana, text="Dame tu altura: ", bg="red")
+etiqueta2.pack()
+entrada2=tk.Entry(ventana)
+entrada2.pack()
+boton=tk.Button(ventana,text="Calcula",command=operacion)
+boton.pack()
+etiqueta_resultado = tk.Label(ventana, text="") 
+etiqueta_resultado.pack()
+ventana.mainloop()
